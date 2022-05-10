0999dd6cc788cd1f4b811f7331de2390d564f878
cottonpup <67526014+cottonpup@users.noreply.github.com>
2022-05-10 22:17:22 +0200

    Add image preview and show 'details' depending on user input (#3)
    * add image preview
    
    * show detail depending on input

--- src/components/Preview/Preview.tsx
+++ src/components/Preview/Preview.tsx
@@ -1,13 +1,35 @@
 // import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { CVData } from '../../hooks/cvDataHook';
 import { useWindowSize } from '../../hooks/windowSizeHook';
 
 interface Props {
   cvData: CVData;
+  fileItem: { url?: string; isUploaded: boolean };
 }
 
 const Preview: React.FC<Props> = (props) => {
   const windowSize = useWindowSize();
+  const [showDetails, setShowDetails] = useState(false);
+
+  useEffect(() => {
+    if (
+      props.cvData.email ||
+      props.cvData.country ||
+      props.cvData.city ||
+      props.cvData.address ||
+      props.cvData.postal_code ||
+      props.cvData.driving_license ||
+      props.cvData.nationality ||
+      props.cvData.place_of_birth ||
+      props.cvData.date_of_birth
+    ) {
+      setShowDetails(true);
+      // TODO 진행률 +8% 올리기
+    } else {
+      setShowDetails(false);
+    }
+  }, [props.cvData]);
 
   return (
     // <div className="w-1/2 p-10 bg-slate-500 fixed right-0 top-0 h-screen flex justify-center flex-col items-center">
@@ -46,19 +68,63 @@ const Preview: React.FC<Props> = (props) => {
       <div className="w-full flex-1 flex items-center justify-center">
         <div className="relative leading-10">
           <div
-            className="bg-white aspect-[21/29.7] h-[1122.52px] rounded-md p-8 absolute"
+            className="bg-white aspect-[21/29.7] h-[1122.52px] rounded-md absolute"
             style={{
               transform: `translate(-50%, -50%) scale(calc(${windowSize.height - 130} /
           1122.52))`,
             }}
           >
-            <h1 className="text-left text-[36px]">{`${props.cvData.first_name} ${props.cvData.last_name}`}</h1>
-            <p className="text-left text-[15px] mb-5">{props.cvData.job_title}</p>
-            <div className="flex items-center">
-              <i className="fa-solid fa-user mr-1"></i>
-              <h2 className="text-left text-[17px] font">Profile</h2>
+            <div className="p-14">
+              <section className="w-full">
+                <div className="flex items-start">
+                  <div
+                    className={`h-20 w-20 bg-cover mr-4 rounded-sm bg-center ${
+                      !props.fileItem.isUploaded ? 'hidden' : ''
+                    }`}
+                    style={{
+                      backgroundImage: `${
+                        props.fileItem.isUploaded ? `url(${props.fileItem.url})` : ''
+                      }`,
+                    }}
+                  ></div>
+                  <div>
+                    <h1 className="text-left text-[36px]">{`${props.cvData.first_name} ${props.cvData.last_name}`}</h1>
+                    <p className="text-left text-[15px] mb-5">{props.cvData.job_title}</p>
+                  </div>
+                </div>
+              </section>
+              <section className="flex">
+                <div className="flex-[3_3_0%]">
+                  <div className="flex items-center">
+                    <i className="fa-solid fa-user mr-1"></i>
+                    <h2 className="text-left text-[17px] font font-semibold">Profile</h2>
+                  </div>
+                </div>
+                <div className="flex-1">
+                  <h2
+                    className={`text-left text-[14px] font-medium leading-tight pt-5 text-cyan-700 ${
+                      !showDetails ? 'hidden' : ''
+                    }`}
+                  >
+                    Details
+                  </h2>
+                  <p className="text-left text-[12px] leading-snug">
+                    {props.cvData.address}
+                  </p>
+                  <p className="text-left text-[12px] leading-snug">
+                    {`${props.cvData.city ? props.cvData.city : ''} ${
+                      props.cvData.postal_code ? `, ${props.cvData.postal_code}` : ''
+                    }`}
+                  </p>
+                  <p className="text-left text-[12px] leading-snug">
+                    {props.cvData.phone}
+                  </p>
+                  <p className="text-left text-[12px] leading-snug">
+                    {props.cvData.email}
+                  </p>
+                </div>
+              </section>
             </div>
-            <h2 className="text-left text-[17px] font">Details</h2>
           </div>
         </div>
       </div>
