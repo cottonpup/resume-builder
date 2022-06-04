9c25ef45510f2daffeede2f8553494030787a586
cottonpup <67526014+cottonpup@users.noreply.github.com>
2022-06-04 11:12:47 +0200

    Implement scroll then fix (#31)

--- src/components/Editor/EditorView.tsx
+++ src/components/Editor/EditorView.tsx
@@ -1,5 +1,5 @@
 import Title from '../UI/Title';
-import ProgressBar from '../UI/ProgressBar';
+import { ProgressBar } from '../UI/ProgressBar';
 import PersonalDetail from './PersonalDetail';
 import { EmploymentHistory } from './EmploymentHistory';
 import { ProfessionalSummary } from './ProfessionalSummary';
@@ -7,14 +7,48 @@ import { Education } from './Education';
 import { WebsiteSocialLink } from './WebsiteSocialLink';
 import { Skills } from './Skills';
 import { Languages } from './Languages';
+import { useEffect, useRef, useState } from 'react';
 
 function Editor() {
+  const [shouldFixHeader, setShouldFixHeader] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const TITLE_HEIGHT_PX = ref.current!.getBoundingClientRect().height;
+
+    function scrollThenFix() {
+      console.log(TITLE_HEIGHT_PX, window.screenTop);
+      if (window.scrollY > TITLE_HEIGHT_PX && !shouldFixHeader) {
+        console.log('FIX HEADER!!');
+        setShouldFixHeader(true);
+      } else if (window.scrollY <= TITLE_HEIGHT_PX && shouldFixHeader) {
+        console.log('UNFIX HEADER!!');
+        setShouldFixHeader(false);
+      }
+    }
+
+    window.addEventListener('scroll', scrollThenFix);
+    return () => {
+      window.removeEventListener('scroll', scrollThenFix);
+    };
+  }, [shouldFixHeader]);
+
   return (
     <section className="flex-1 p-[48px] max-w-full xl:w-2/4">
       <div className="max-w-[760px] m-auto xl:m-0 xl:max-w-full">
-        <section>
+        <section ref={ref}>
           <Title>Simple Resume</Title>
-          <ProgressBar />
+          <section
+            className={`${
+              shouldFixHeader
+                ? 'fixed top-0 w-[50%] px-[48px] left-0 pt-[20px] bg-white z-10'
+                : ''
+            }`}
+          >
+            <ProgressBar />
+          </section>
+        </section>
+        <section>
           <PersonalDetail />
         </section>
         <section>
