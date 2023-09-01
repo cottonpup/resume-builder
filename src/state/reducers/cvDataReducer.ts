771e13a888ea32816fd8c5cc2059034ed39d301f
cottonpup <younggyoung.lee1@gmail.com>
2023-09-01 14:53:47 +0900

    Initialize the default state

--- src/state/reducers/cvDataReducer.ts
+++ src/state/reducers/cvDataReducer.ts
@@ -92,6 +92,7 @@ const initialStateSampleCV = {
         place_of_birth: '',
         date_of_birth: '',
     },
+    // FIXME Why can't I remove this?
     professional_summary: {
         blocks: [
             {
@@ -107,134 +108,11 @@ const initialStateSampleCV = {
         //  TODO: What was entityMap?
         entityMap: {},
     } as RawDraftContentState,
-    employment_history: [
-        {
-            id: '8888c05f-e071-4d4b-9b67-970ec6f32fb1',
-            job_title: 'Software Engineer',
-            employer: 'Lime',
-            city: 'Greenland',
-            description: {
-                blocks: [
-                    {
-                        key: '9e8iv',
-                        text: 'Citric =)',
-                        type: 'unstyled',
-                        depth: 0,
-                        inlineStyleRanges: [],
-                        entityRanges: [],
-                        data: {},
-                    },
-                ],
-                entityMap: {},
-            },
-            startYear: 2722,
-            startMonth: 'Jan',
-            endYear: 2522,
-            endMonth: 'Jul',
-        },
-    ] as EmploymentElement[],
-    education: [
-        // {
-        // id: 'baa4f9e5-f66f-4244-99c3-1261a04e1b21',
-        // school: 'University of Twitter',
-        // degree: 'Bachelor of Software Engineering',
-        // city: 'Seoul, Korea',
-        // description: {
-        //     blocks: [
-        //             {
-        //                 key: 'ec0di',
-        //                 text: '',
-        //                 type: 'unstyled',
-        //                 depth: 0,
-        //                 inlineStyleRanges: [],
-        //                 entityRanges: [],
-        //                 data: {},
-        //             },
-        //         ],
-        //         entityMap: {},
-        //     },
-        //     startYear: 2013,
-        //     startMonth: 'Sep',
-        //     endYear: 2018,
-        //     endMonth: 'Sep',
-        // },
-        // {
-        //     id: 'aaa4f9e6-f66f-4244-99c3-1261a04e1b21',
-        //     school: 'Blabla UNI',
-        //     degree: 'Bachelor of Software Engineering',
-        //     city: 'Seoul, Korea',
-        //     description: {
-        //         blocks: [
-        //             {
-        //                 key: 'ec0di',
-        //                 text: '',
-        //                 type: 'unstyled',
-        //                 depth: 0,
-        //                 inlineStyleRanges: [],
-        //                 entityRanges: [],
-        //                 data: {},
-        //             },
-        //         ],
-        //         entityMap: {},
-        //     },
-        //     startYear: 2413,
-        //     startMonth: 'Sep',
-        //     endYear: 2418,
-        //     endMonth: 'Sep',
-        // },
-    ] as EducationElement[],
-    websites_social_links: [
-        // {
-        //     id: '812e8838-b0ef-451a-995d-01dc4e324fa0',
-        //     label: 'github.com/cottonpup',
-        //     link: 'github.com/cottonpup',
-        // },
-        // {
-        //     id: 'd599b9d2-52fd-4752-9e63-e540d927a28c',
-        //     label: 'linkedin.com/in/cottonpup',
-        //     link: '',
-        // },
-    ] as WebsiteSocialElement[],
-    skills: [
-        // {
-        //     id: '261c7883-398c-46a6-b4b7-0cd8dce8fe44',
-        //     skill: 'Bootstrap',
-        // },
-        // {
-        //     id: '99d2cf5c-9045-469e-b574-c0e8d25be221',
-        //     skill: 'CSS3',
-        // },
-        // {
-        //     id: '55bf6497-99ae-456c-9f6b-b5839d228d06',
-        //     skill: 'Adaptability',
-        // },
-        // {
-        //     id: 'bf4f57e7-76e6-4315-ad12-9c12d640595c',
-        //     skill: 'Business Strategy',
-        // },
-        // {
-        //     id: '4aa484ce-80ce-4442-a2cb-3cb6dda5c60d',
-        //     skill: 'Customer Service',
-        // },
-        // {
-        //     id: '72b3c735-be1a-456d-b7d8-104b8e3461b9',
-        //     skill: 'High Attention to Detail',
-        // },
-    ] as SkillsElement[],
-    languages: [
-        // {
-        //     id: 'ac8992eb-5441-4a6b-8085-a145ef72b36e',
-        //     language: 'English',
-        //     level: 'Highly proficient',
-        //     proficiency: 80,
-        // },
-        // {
-        //     id: '172a4def-1b82-493b-905f-89be9b39132b',
-        //     language: 'Korean',
-        //     level: 'Native speaker',
-        //     proficiency: 0,
-        // },
-    ] as LanguagesElement[],
+    employment_history: [] as EmploymentElement[],
+    education: [] as EducationElement[],
+    websites_social_links: [] as WebsiteSocialElement[],
+    skills: [] as SkillsElement[],
+    languages: [] as LanguagesElement[],
 };
 
 export type CVData = typeof initialState;
